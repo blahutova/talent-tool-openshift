f27739b03a2afde3455dd6a44c50b95f81d695a3
lblahuto <lblahuto@redhat.com>
2019-01-16 13:41:28 +0100

    configure cors for deploy

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000'
+    origins 'http://talent-frontend-talent-tool.int.open.paas.redhat.com/'
 
     resource '*',
       headers: :any,
